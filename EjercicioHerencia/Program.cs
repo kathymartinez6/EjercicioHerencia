94df7deb87114c91739556c658cdc50015f6b5bb
kathy_chell <chela.lara76@gmail.com>
2024-10-23 21:36:26 -0600

    2

--- EjercicioHerencia/Program.cs
+++ EjercicioHerencia/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void Main(string[] args)
+{
+    // Instancia de la clase Calculos
+    Calculos calculos = new Calculos();
+
+    // Realizando los cálculos
+    Console.WriteLine("Cálculo de área del círculo:");
+    calculos.CalcularAreaCirculo(5); // Ejemplo de radio = 5
+
+    Console.WriteLine("\nCálculo de volumen del cubo:");
+    calculos.CalcularVolumenCubo(3); // Ejemplo de lado = 3
+
+    Console.WriteLine("\nCálculo de longitud de la circunferencia:");
+    calculos.CalcularLongitudCircunferencia(7); // Ejemplo de radio = 7
+}
+
